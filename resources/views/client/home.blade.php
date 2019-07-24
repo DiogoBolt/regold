e49909799ed983fd0c12df64606ea760545e1bb2
Manuel Silva <manelafsilva@gmail.com>
2019-07-24 22:31:17 +0100

    created client index css

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,36 +1,43 @@
 @extends('layouts.frontoffice')
 
+@section('styles')
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/index/client-index.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-xs-4">
-            <a title="Produtos" href="/frontoffice/produtos"><img style="width:100%" src="{{ URL::to('/') }}/img/produtos.jpg"></a>
-        </div>
-
-        <div class="col-xs-4">
-            <a title="Encomendas" href="/frontoffice/orders"><img style="width:100%" src="{{ URL::to('/') }}/img/encomendas.jpg"></a>
-        </div>
-
-        <div class="col-xs-4">
-            <a title="Mensagens" href="/frontoffice/messages"><img style="width:100%" src="{{ URL::to('/') }}/img/mensagens.jpg"></a>
-        </div>
-
-
+<div class="container index-box">
+    <div class="box">
+        <a title="Encomendas" href="/frontoffice/orders">
+            <img class="img-responsive" src="{{ URL::to('/') }}/img/index/icon1.png">
+        </a>
+        <div class="desc">ENCOMENDAS</div>
     </div>
 
-    <div class="row" style="margin-top:30px">
-
-        <div class="col-xs-4">
-            <a title="Documentos Contabilisticos" href="/frontoffice/documents/Contabilistico"><img style="width:100%" src="{{ URL::to('/') }}/img/relatorios.jpg"></a>
-        </div>
-        <div class="col-xs-4">
-            <a title="Documentos Pragas" href="/frontoffice/documents/Controlopragas"><img style="width:100%" src="{{ URL::to('/') }}/img/controlodepragas.jpg"></a>
-        </div>
-
-        <div class="col-xs-4">
-            <a title="Documentos HACCP" href="/frontoffice/documents/HACCP"><img style="width:100%" src="{{ URL::to('/') }}/img/HACCP.jpg"></a>
-        </div>
+    <div class="box">
+        <a title="Documentos HACCP" href="/frontoffice/documents/HACCP"><img class="img-responsive" src="{{ URL::to('/') }}/img/index/icon2.png"></a>
+        <div class="desc">DOCUMENTOS HACCP</div>
     </div>
-        </div>
+
+    <div class="box">
+        <a title="Controlo de Pragas" href="/frontoffice/documents/Controlopragas"><img class="img-responsive" src="{{ URL::to('/') }}/img/index/icon3.png"></a>
+        <div class="desc">CONTROLO DE PRAGAS</div>
+    </div>
+
+    <div class="box">
+        <a title="Agenda" href="#"><img class="img-responsive" src="{{ URL::to('/') }}/img/index/icon4.png"></a>
+        <div class="desc">AGENDA</div>
+    </div>
+
+    <div class="box">
+        <a title="Pagamentos" href="#"><img class="img-responsive" src="{{ URL::to('/') }}/img/index/icon5.png"></a>
+        <div class="desc">PAGAMENTOS</div>
+    </div>
+
+    <i class="box" aria-hidden="true"></i>
+
+{{-- <a title="Produtos" href="/frontoffice/produtos"><img style="width:100%" src="{{ URL::to('/') }}/img/produtos.jpg"></a>
+<a title="Mensagens" href="/frontoffice/messages"><img style="width:100%" src="{{ URL::to('/') }}/img/mensagens.jpg"></a>
+<a title="Documentos Contabilisticos" href="/frontoffice/documents/Contabilistico"><img style="width:100%" src="{{ URL::to('/') }}/img/relatorios.jpg"></a> --}}
 
 @endsection
