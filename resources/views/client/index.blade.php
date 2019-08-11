6e0105fa487044a0a9d12fe18f6a74fc13dd3297
Manuel Silva <manelafsilva@gmail.com>
2019-08-11 23:40:00 +0100

    lots of stuff

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,39 +1,38 @@
 @extends('layouts.app')
 
+<link href="{{ asset('css/client/client-bo.css') }}" rel="stylesheet">
+
 @section('content')
+<div class="container-bar">
+    <p class="container-bar_txt">clientes</p>
+    <div class="container-bar_img">
+        <img src="{{ asset('img/clientes.jpg') }}" />
+    </div>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Clientes</div>
-
-                <div class="panel-body">
+            <div class="panel">
+                <div class="panel-body table-responsive">
                     <form method="get" action="/clients">
-                        <input name="search" class="form-control">
-                        <button type="submit" class="btn btn-primary">Pesquisa</button>
-
-                    </form>
-
-                    <table class="table table-bordered">
+                        <input id="client-search" name="search" class="form-control" placeholder="Pesquisa de clientes.." />
+                     </form>
+                    <table class="table">
                         <tr>
                             <th>Id</th>
                             <th>Nome</th>
                             <th>Grupo</th>
-
                         </tr>
                         @foreach($clients as $client)
                             <tr>
                                 <td><a href="/clients/{{$client->id}}">{{$client->id}}</a></td>
                                 <td><a href="/clients/{{$client->id}}">{{$client->name}}</a></td>
                                 <td><a href="/groups/{{$client->groupid}}">{{$client->group}}</a></td>
-
                             </tr>
-
                         @endforeach
-
                     </table>
 
-                    <a href="/clients/new" class="btn btn-success">Novo Cliente</a>
+                    <a href="/clients/new" class="btn btn-add"><strong>Novo Cliente</strong></a>
                 </div>
             </div>
         </div>
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,84 +1,106 @@
 @extends('layouts.app')
 
+<link href="{{ asset('css/orders/orders-bo.css') }}" rel="stylesheet">
+
 @section('content')
+<div class="container-bar">
+    <p class="container-bar_txt">encomendas</p>
+    <div class="container-bar_img">
+        <img src="{{ asset('img/encomendas.jpg') }}" />
+    </div>
+</div>
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Encomendas</div>
-
-                <div class="panel-body">
-                    <h5 style="color:darkorange">Em Espera</h5>
-                    <table class="table table-bordered">
+        <div class="panel">
+            <div class="panel-body table-responsive">
+                <h5 style="color:darkorange">Em Espera</h5>
+                <table class="table">
+                    <tr>
+                        <th>ID Cliente</th>
+                        <th>Total</th>
+                        <th>Total + Iva</th>
+                        <th>Estado</th>
+                        <th>Fatura</th>
+                        <th>Detalhes</th>
+                    </tr>
+                    @foreach($orders as $order)
+                        @if($order->processed == 0)
                         <tr>
-                            <th>ID Cliente</th>
-                            <th>Total</th>
-                            <th>Total + Iva</th>
-                            <th>Estado</th>
-                            <th>Fatura</th>
-                            <th>Detalhes</th>
+                            <td><a href="/clients/{{$order->client_id}}">{{$order->client_id}}</a></td>
+                            <td>{{number_format($order->total,2)}}€</td>
+                            <td>{{number_format($order->totaliva,2)}}€</td>
+                            <td>Em Espera</td>
+                            @if($order->receipt_id == null)
+                                <td class="form-td">
+                                <form action="/orders/attachReceipt" class="order-form" method="post" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
 
+                                    <input value="{{$order->id}}" type="hidden" name="order">
 
-                        </tr>
-                        @foreach($orders as $order)
-                            @if($order->processed == 0)
+                                    <label for="{{$order->id}}" class="btn"><strong>Adicionar Factura</strong></label>
+                                    <input id="{{$order->id}}" class="input-order" type="file" name="receipt">
+                                    
+                                    <button class="btn">Associar</button>
+                                </form>
+                                </td>
+                            @else
+                                <td>FALTA</td>
+                            @endif
+                            <td><a href="/orders/{{$order->id}}">Detalhes</a></td>
+                            </tr>
+
+                    @endif
+                    @endforeach
+
+                </table>
+                <h5 style="color:green">Processadas</h5>
+
+                <table class="table">
+                    <tr>
+                        <th>ID Cliente</th>
+                        <th>Total</th>
+                        <th>Total + Iva</th>
+                        <th>Estado</th>
+                        <th>Detalhes</th>
+
+                    </tr>
+                    @foreach($orders as $order)
+                        @if($order->processed == 1)
                             <tr>
                                 <td><a href="/clients/{{$order->client_id}}">{{$order->client_id}}</a></td>
                                 <td>{{number_format($order->total,2)}}€</td>
                                 <td>{{number_format($order->totaliva,2)}}€</td>
-                                <td>Em Espera</td>
-                                @if($order->receipt_id == null)
-                                    <td>
-                                    <form action="/orders/attachReceipt"  method="post" enctype="multipart/form-data">
-                                        {{ csrf_field() }}
+                                <td>Processado</td>
 
-                                        <input class="form-control" type="file" name="receipt">
-                                        <input value="{{$order->id}}" type="hidden" name="order">
-                                        <button class="btn btn-success">Adicionar Fatura</button>
-                                    </form>
-                                    </td>
-                                @else
-                                    <td>FALTA</td>
-                                @endif
                                 <td><a href="/orders/{{$order->id}}">Detalhes</a></td>
-                                </tr>
-
+                            </tr>
                         @endif
-                        @endforeach
+                    @endforeach
 
-                    </table>
-
-
-                    <h5 style="color:green">Processadas</h5>
-
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>ID Cliente</th>
-                            <th>Total</th>
-                            <th>Total + Iva</th>
-                            <th>Estado</th>
-                            <th>Detalhes</th>
-
-                        </tr>
-                        @foreach($orders as $order)
-                            @if($order->processed == 1)
-                                <tr>
-                                    <td><a href="/clients/{{$order->client_id}}">{{$order->client_id}}</a></td>
-                                    <td>{{number_format($order->total,2)}}€</td>
-                                    <td>{{number_format($order->totaliva,2)}}€</td>
-                                    <td>Processado</td>
-
-                                    <td><a href="/orders/{{$order->id}}">Detalhes</a></td>
-                                </tr>
-                            @endif
-                        @endforeach
-
-                    </table>
-                </div>
+                </table>
             </div>
         </div>
     </div>
 </div>
 
-
 @endsection
+
+<script>
+
+document.addEventListener('DOMContentLoaded', function() { 
+
+    $('.input-order').change( function() {
+
+        let file = $(this)[0].files[0];
+
+        if(file) {
+            $(this).prev('label').text(file.name);
+        } else {
+            $(this).prev('label').text('Adicionar Factura');
+        }
+        
+    });
+
+});
+
+</script>
