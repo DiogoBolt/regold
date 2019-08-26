95d928d98c013b9665ab6aa06690d96cf23cd7dc
Manuel Silva <manuel.silva@sfponline.pt>
2019-08-26 11:11:55 +0100

    cart fix

--- resources/views/frontoffice/cart.blade.php
+++ resources/views/frontoffice/cart.blade.php
@@ -9,7 +9,7 @@
     <div class="container-bar">
         <p class="container-bar_txt">carrinho</p>
         <div class="container-bar_img">
-            <img src="{{ asset('img/cart.png') }}" />
+            <img src="{{ asset('img/cart.png') }}"/>
         </div>
     </div>
 
@@ -22,42 +22,42 @@
     <div class="container">
         @if(count($line_items) > 0 )
             <div class="cart-container">
-                    @foreach($line_items as $item)
-                        <div class="cart-item" style="width:50%">
-                            <div class="cart-item_img">
-                                <span class="price-tag">{{number_format($item->total,2)}}€</span>
-                                <img src="/uploads/products/{{$item->product->file}}">
-                            </div>
-                            
-                            <div class="cart-item_desc">
-                                <h3>{{$item->product->name}}</h3>
-                                <div class="cart-item_desc-txt">
-                                    
-                                       {{$item->product->details}}
-                        
-                                </div>
-                            </div>    
-                
-                            <div class="cart-item_extra">
-                                <a href="/frontoffice/cart/delete/{{$item->id}}">remover x</a>
-                                <p>Quantidade:{{$item->amount}}</p>
-                            </div>                    
+                @foreach($line_items as $item)
+                    <div class="cart-item">
+                        <div class="cart-item_img">
+                            <span class="price-tag">{{number_format($item->total, 2 , '.', '')}} €</span>
+                            <img src="/uploads/products/{{$item->product->file}}">
                         </div>
-                    @endforeach     
+
+                        <div class="cart-item_desc">
+                            <h3>{{$item->product->name}}</h3>
+                            <div class="cart-item_desc-txt">
+
+                                {{$item->product->details}}
+
+                            </div>
+                        </div>
+
+                        <div class="cart-item_extra">
+                            <a href="/frontoffice/cart/delete/{{$item->id}}">remover x</a>
+                            <p>Quantidade:{{$item->amount}}</p>
+                        </div>
+                    </div>
+                @endforeach
             </div>
 
             <div class="cart-info">
-                <p>Total : {{$totalprod}}€</p>
+                <p>Total : {{number_format($totalprod, 2, '.', '')}}€</p>
                 @foreach($items as $item)
-                    <p>{{$item['descr']}} : {{$item['amount']}}€</p>
+                    <p>{{$item['descr']}} : {{number_format($item['amount'], 2, '.','')}} €</p>
                 @endforeach
-                <h4>Total : {{number_format($total,2)}}€</h4>
+                <h4>Total IVA : {{number_format($total,2, '.', '')}} €</h4>
 
             </div>
-     
+
 
             <a href="/frontoffice/cart/process" class="btn btn-cart">validar carrinho</a>
-        @else 
+        @else
 
             <div class="cart-container">
                 <h1>O carrinho encontra-se vazio.</h1>
