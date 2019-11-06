ca0471fd3f6d6f2ca820b3625d7d77c4d78a830a
Diogo.Araujo <diogo.araujo@sfponline.pt>
2019-11-06 17:26:56 +0000

    tirar registo

--- resources/views/layouts/frontoffice.blade.php
+++ resources/views/layouts/frontoffice.blade.php
@@ -66,7 +66,6 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li>
                                 <a href="{{ route('invoices') }}">Facturas</a>
