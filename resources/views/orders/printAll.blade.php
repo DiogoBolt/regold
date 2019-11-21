1daa691c4917851cb293a50bfbb1bffe1f84f798
Manuel Silva <manelafsilva@gmail.com>
2019-11-21 20:23:31 +0000

    print css changes

--- resources/views/orders/printAll.blade.php
+++ resources/views/orders/printAll.blade.php
@@ -2,64 +2,54 @@
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link href="{{ asset('css/orders/order.css') }}" rel="stylesheet">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel">
-                    <div class="panel-body table-responsive">
-    
-                        <div style="text-align: center; margin-bottom: 20px;">
-                            <img src="/img/navbar/logoindexcolor.png"/>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel">
+                <div class="panel-body table-responsive printall">
+
+                    <div style="text-align: center; margin-bottom: 20px;">
+                        <img src="/img/navbar/logoindexcolor.png"/>
+                    </div>
+
+                    @foreach ($printingData as $item)
+                        <div class="row">
+                            <div class="print_details">
+                                <div>
+                                    <b>NOME : </b>{{ $item['client']['name'] }}
+                                </div>
+                                <div>
+                                    <b>MORADA
+                                        : </b>{{ $item['client']['address'] == null ? $item['client']['address'] : $item['client']['invoice_address'] }}
+                                </div>
+                                <div>
+                                    <b>METODO PAGAMENTO : </b>{{ $item['client']['payment_method'] }}
+                                </div>
+                                <div>
+                                    <b>CONTA CORRENTE : </b>{{ $item['client']['total'] }}
+                                </div>
+                            </div>
+                            <div class="print_details">
+                                <div>
+                                    <b>REGOLD ID : </b>{{ $item['client']['regoldiID'] }}
+                                </div>
+                                <div>
+                                    <b>CRIADA A : </b>{{ $item['order']['created_at'] }}
+                                </div>
+                                <div>
+                                    <b>TOTAL : </b>{{ $item['order']['total'] }}
+                                </div>
+                                <div class=" print-checkbox">
+                                    <b>PREPARADO : </b>
+                                    <label>
+                                        <input type="checkbox"/>
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
 
-                        @foreach ($printingData as $item)
-                            <div class="row">
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>NOME : </b>{{ $item['client']['name'] }}
-                                    </div>
-                                </div>
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>MORADA : </b>{{ $item['client']['address'] == null ? $item['client']['address'] : $item['client']['invoice_address'] }}
-                                </div>
-                                </div>
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>METODO PAGAMENTO : </b>{{ $item['client']['payment_method'] }}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>CONTA CORRENTE : </b>{{ $item['client']['total'] }}
-                                    </div>
-                                </div>
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>REGOLD ID : </b>{{ $item['client']['regoldiID'] }}
-                                    </div>
-                                </div>
-                                <div class="col-sm" style="display:inline">
-                                    <div class="form-group">
-                                        <b>CRIADA A : </b>{{ $item['order']['created_at'] }}
-                                    </div>
-                                </div>
-                                    <div class="form-group">
-                                        <b>TOTAL : </b>{{ $item['order']['total'] }}
-                                    </div>
-                                </div>
-
-
-                            <div class="form-group print-checkbox">
-                                <b>PREPARADO : </b>
-                                <label>
-                                    <input type="checkbox" />
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
+                        <div class="row">
 
                             <table class="table table-bordered">
                                 <tr>
@@ -80,20 +70,22 @@
                             </table>
 
                             <hr>
-                        @endforeach
-                        
-                    </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
-                
+</div>
+
 <script>
 
-    window.onload = function() {
-       
+    window.onload = function () {
+
         setTimeout(window.print, 500);
-        setTimeout(function () { window.close(); }, 500); 
+        setTimeout(function () {
+            window.close();
+        }, 500);
     };
 
 </script>
