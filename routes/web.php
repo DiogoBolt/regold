26729e4c9ecd92947e240ab3f4040eece5648bc8
Diogo <diogo.araujo@gmail.com>
2021-02-25 17:24:54 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::post('/api/confirm/', 'ApiController@confirmPayment');
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/duvidascovid', 'HomeController@duvidascovid')->name('covid');
+Route::get('/dbxprice', 'HomeController@dbxprice');
 
 
 Auth::routes(['register' => false]);
