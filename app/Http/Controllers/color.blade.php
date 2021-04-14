af2296d5221b83b371c07ed8f6c067be9f5a1dc1
Diogo <diogo.araujo@gmail.com>
2021-04-14 22:25:45 +0100

    timezone

---
+++ app/Http/Controllers/color.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+
+</head>
+<body class="header-fixed">
+<h1 align="center"> Choose Color</h1>
+<br><br>
+<div align="center">
+<a href="/changeColor/Red" style="color:red"><h1>RED</h1></a>
+<a href="/changeColor/Green" style="color:Green"><h1>Green</h1></a>
+<a href="/changeColor/Blue" style="color:Blue"><h1>Blue</h1></a>
+</div>
+</body>
+</html>
+
